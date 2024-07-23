1b2005cd705ddce35a8799c7dcb2135531facc04
tidy <xu_yanfeng@126.com>
2024-07-23 07:35:23 +0000

    add-ndk

---
+++ ndk.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# install dep
+sudo apt-get install gcc automake autoconf libtool make gcc-multilib
+
+# download ndk
+version=r12b
+ndk_dir=$(pwd)/android-ndk-${version}
+if [ ! -d "${ndk_dir}" ]; then
+    echo "not exists ndk"
+    echo "ndk downloading ..."
+    wget https://dl.google.com/android/repository/android-ndk-${version}-linux-x86_64.zip
+    echo "unzip ndk"
+    unzip -o android-ndk-${version}-linux-x86_64.zip -d ./
+fi
+echo "ndk_dir:${ndk_dir}"
+
+script=build-${version}-linux.sh
+chmod +x ./${script}
+# ./${script}
